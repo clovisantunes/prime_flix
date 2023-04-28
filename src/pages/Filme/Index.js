6eb668784f7f6667628128973f221afe822d58da
Clovis antunes <clovissantanna@gmail.com>
2023-04-28 16:16:16 -0300

    iniciando a pagina de detalhes

--- src/pages/Filme/Index.js
+++ src/pages/Filme/Index.js
@@ -1,11 +1,50 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from '../../services/api';
 function Filme () {
     const {id} = useParams();
+    const[filme,setFilme] = useState({});
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key:"a37b48df5a015c0db67c6000097432aa",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme não Encontrado")
+            })
+        }
+        loadFilme();
+
+        return() => {
+            console.log("componente desmontado")
+        }
+    },[])
+
+    if (loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando Detalhes...</h1>
+            </div>
+        )
+    }
     return(
-        <div>
-            <h1>Acessando Filme {id}</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+           <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} /10</strong>
         </div>
     )
 }
